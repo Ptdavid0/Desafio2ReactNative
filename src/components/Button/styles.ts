79ce28928a62a1d0191e6ba00362da35c12d6ec4
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-14 17:24:21 -0300

    Button concluded

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -15,25 +15,28 @@ export const Container = styled.TouchableOpacity<Props>`
   max-height: 56px;
 
   background-color: ${({ theme, type }) =>
-    type === "PRIMARY" ? theme.COLORS.GRAY_600 : theme.COLORS.RED_DARK};
+    type === "PRIMARY" ? theme.COLORS.GRAY_600 : theme.COLORS.WHITE};
 
   border-radius: 8px;
 
   justify-content: center;
   align-items: center;
+  border: 2px solid;
+  border-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GRAY_600 : theme.COLORS.GRAY_700};
 `;
 
-export const Title = styled.Text`
-  ${({ theme }) => css`
+export const Title = styled.Text<Props>`
+  ${({ theme, type }) => css`
     font-family: ${theme.FONT_FAMILY.BOLD};
     font-size: ${theme.FONT_SIZE.MD}px;
-    color: ${theme.COLORS.WHITE};
+    color: ${type === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_700};
   `}
 `;
 
 export const Icon = styled(MaterialIcons).attrs<Props>(({ theme, type }) => ({
   size: 24,
-  color: theme.COLORS.WHITE,
+  color: type === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_700,
 }))`
   margin-right: 8px;
 `;
