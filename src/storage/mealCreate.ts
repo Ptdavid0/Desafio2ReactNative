2eedd7170b6c6089903939bd3a4b416bf416b035
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-21 13:40:49 -0300

    removing unnused code

--- src/storage/mealCreate.ts
+++ src/storage/mealCreate.ts
@@ -65,7 +65,6 @@ export const createMeal = async (meal: Meal) => {
 
       await AsyncStorage.setItem(COLLECTION_MEALS, JSON.stringify([newGroup]));
     }
-    const updatedStorage = await AsyncStorage.getItem(COLLECTION_MEALS);
   } catch (error: any) {
     throw new Error(error);
   }
